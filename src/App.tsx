94f37adc10fec6bf9fbbcb12e7a852c945728f76
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-06-22 21:55:00 +0530

    removed unnecessary comment

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="text-3xl font-bold text-center mt-10 text-blue-600">
-//       Hello Vite + React + Tailwind!
-//     </div>
-//   );
-// }
-
-// export default App;
