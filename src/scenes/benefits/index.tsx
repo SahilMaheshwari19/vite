ef6ef0ad3aaf81b587992bb69a6138fea7fb0c28
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-06-27 21:43:13 +0530

    add animation in benefit component

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -29,6 +29,13 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -40,15 +47,33 @@ const Benefits = ({ setSelectedPage }: Props) => {
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
         {/* HEADER */}
-        <HText>MORE THAN JUST A GYM</HText>
-        <p className="my-5 text-sm">
-          We provide world class fitness equipment, trainers and classes to get
-          you to your ultimate fitness goals with ease. We provide true care
-          into each and every member.
-        </p>
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST A GYM</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
       </motion.div>
       {/* BENEFITS */}
-      <motion.div className="md:flex items-center justify-between gap-8 mt-5">
+      <motion.div
+        className="md:flex items-center justify-between gap-8 mt-5"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={container}
+      >
         {benefits.map((benefit: BenefitType) => (
           <Benefit
             key={benefit.title}
