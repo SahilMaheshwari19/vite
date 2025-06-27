53e629a6b299a1208fd749f61761577c42caec88
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-06-27 22:14:32 +0530

    add graphics , text and images with animation in benefit component

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -7,6 +7,8 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 const benefits: Array<BenefitType> = [
   {
@@ -65,24 +67,92 @@ const Benefits = ({ setSelectedPage }: Props) => {
             care into each and every member.
           </p>
         </motion.div>
-      </motion.div>
-      {/* BENEFITS */}
-      <motion.div
-        className="md:flex items-center justify-between gap-8 mt-5"
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        variants={container}
-      >
-        {benefits.map((benefit: BenefitType) => (
-          <Benefit
-            key={benefit.title}
-            icon={benefit.icon}
-            title={benefit.title}
-            description={benefit.description}
-            setSelectedPage={setSelectedPage}
+        {/* BENEFITS */}
+        <motion.div
+          className="md:flex items-center justify-between gap-8 mt-5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+
+        {/* GRAPHICS AND DESCRIPTION */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* GRAPHIC */}
+          <img
+            className="mx-auto"
+            alt="benefits-page-graphic"
+            src={BenefitsPageGraphic}
           />
-        ))}
+          {/* DESCRIPTION */}
+          <div>
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-[var(--content-abstractwaves)]">
+                <div className="relative">
+                  <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                      hidden: { opacity: 0, x: 50 },
+                      visible: { opacity: 1, x: 0 },
+                    }}
+                  >
+                    <HText>
+                      MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                      <span className="text-primary-500">FIT</span>
+                    </HText>
+                  </motion.div>
+                </div>
+              </div>
+            </div>
+            {/* TEXT */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 1 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A error
+                laudantium necessitatibus voluptate architecto nulla assumenda
+                cum voluptatibus nihil, soluta quibusdam accusantium porro
+                tempora, eveniet aperiam, similique inventore! Odit, saepe.
+              </p>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur aspernatur doloribus quaerat eligendi non
+                consectetur fugit similique, repudiandae praesentium ad,
+                inventore atque nesciunt itaque dicta, vitae provident.
+                Inventore, perferendis eius.
+              </p>
+            </motion.div>
+            {/* BUTTON */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[1] before:content-[var(--content-sparkles)]">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
